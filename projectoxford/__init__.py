4b8c368756a01b6fe1d32c962f55ff9593d0c814
Ivan R. Judson <irjudson@gmail.com>
2015-10-10 20:37:33 -0700

    Moving to __all__ for pep8/pyflakes cleanup

--- projectoxford/__init__.py
+++ projectoxford/__init__.py
@@ -1,6 +1,1 @@
-from .Client import Client
-from .Base import Base
-from .Face import Face
-from .Person import Person
-from .PersonGroup import PersonGroup
-from .Vision import Vision
+__all__ = [ 'Base', 'Client', 'Face', 'Person', 'PersonGroup', 'Vision']
