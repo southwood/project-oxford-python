9750b43243932adc0574b307681d424807852285
Scott Southwood <scsouthw@microsoft.com>
2015-11-26 10:57:48 -0800

    update version number

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.1.3',
+    version='0.1.4',
 
     description='This project extends the Project Oxford API surface to support Python.',
     long_description=long_description,
